e2d6de409ff478548919df4ba5beba68aaff08c4
Rivu Chattopadhyay <rivu861@gmail.com>
2025-06-17 02:24:25 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -34,12 +34,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <ErrorBoundary>
         <AppContextProvider>
           <App />
         </AppContextProvider>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
